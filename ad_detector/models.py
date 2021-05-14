d8b3c91bdc378bfff04d02f976e6946b2b923494
shirly-24 <56828263+shirly-24@users.noreply.github.com>
2021-05-14 19:27:03 +0800

    add textcnn model

--- ad_detector/models.py
+++ ad_detector/models.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 class BiLSTM(nn.Module):
@@ -38,3 +39,34 @@ class BiLSTM(nn.Module):
         x = self.l_r_stack(x)
         x = self.softmax(x)
         return x
+
+
+class TextCNN(nn.Module):
+    def __init__(
+            self,
+            vocab_size: int,
+            embedding_size: int,
+            hidden_size: int,
+            filter_size: int,
+            dropout: float
+    ):
+        super().__init__()
+        self.embedding = nn.Embedding(vocab_size, embedding_size)
+        self.convs = nn.ModuleList(
+            [nn.Conv1d(embedding_size, hidden_size, k) for k in filter_size])
+        self.dropout = nn.Dropout(dropout)
+        self.fc = nn.Linear(hidden_size * len(filter_size), 2)
+
+    def conv_and_pool(self, x, conv):
+        x = F.relu(conv(x))
+        x = F.max_pool1d(x, x.size(2)).squeeze()
+        return x
+
+    def forward(self, x):
+        emb = self.embedding(x)
+        emb = self.dropout(emb)
+        emb = emb.transpose(1, 2)
+        out = torch.cat([self.conv_and_pool(emb, conv) for conv in self.convs], -1)
+        out = self.fc(out)
+        out = F.log_softmax(out, dim=-1)
+        return out
